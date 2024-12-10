0108b3e59bfdc7a9aae343921dc368207d1e4359
pterror <pterrorbird@gmail.com>
2024-12-10 22:13:41 +1000

    feat: optimize day 2 part 2

--- src/aoc2024/day2.rs
+++ src/aoc2024/day2.rs
@@ -4,6 +4,21 @@ use anyhow::Result;
 
 use crate::util::*;
 
+fn is_valid(x: &[i8]) -> bool {
+    let sign = (x[1] - x[0]).signum();
+    for (i, n) in x.iter().skip(1).enumerate() {
+        let diff = n - x[i];
+        if diff.signum() != sign {
+            return false;
+        }
+        let abs_diff = diff.abs();
+        if abs_diff < 1 || abs_diff > 3 {
+            return false;
+        }
+    }
+    true
+}
+
 pub struct Day2;
 
 impl Solution for Day2 {
@@ -27,19 +42,10 @@ impl Solution for Day2 {
 
     fn p1(xs: Self::Input) -> Result<impl Debug> {
         let mut count = 0;
-        'outer: for x in xs {
-            let sign = (x[1] - x[0]).signum();
-            for (i, n) in x.iter().skip(1).enumerate() {
-                let diff = n - x[i];
-                if diff.signum() != sign {
-                    continue 'outer;
-                }
-                let abs_diff = diff.abs();
-                if abs_diff < 1 || abs_diff > 3 {
-                    continue 'outer;
-                }
+        for x in xs {
+            if is_valid(&x) {
+                count += 1;
             }
-            count += 1;
         }
         Ok(count)
     }
@@ -48,21 +54,16 @@ impl Solution for Day2 {
         let mut count = 0;
         'outer: for x in xs {
             for (i, _) in x.iter().enumerate() {
-                let x = x.iter().take(i).chain(x.iter().skip(i + 1)).collect_vec();
-                let diffs = x
+                let x = x
                     .iter()
-                    .skip(1)
-                    .enumerate()
-                    .map(|(i, n)| *n - x[i])
+                    .take(i)
+                    .chain(x.iter().skip(i + 1))
+                    .map(|x| *x)
                     .collect_vec();
-                if !diffs.iter().all(|n| *n < 0) && !diffs.iter().all(|n| *n > 0) {
-                    continue;
+                if is_valid(&x) {
+                    count += 1;
+                    continue 'outer;
                 }
-                if !diffs.iter().all(|n| n.abs() >= 1 && n.abs() <= 3) {
-                    continue;
-                }
-                count += 1;
-                continue 'outer;
             }
         }
         Ok(count)
