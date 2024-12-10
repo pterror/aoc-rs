225f1889fb482d0fcf68d7efb41c4f0bf15529cb
pterror <pterrorbird@gmail.com>
2024-12-10 22:09:37 +1000

    feat: skill issue

--- src/aoc2024/day2.rs
+++ src/aoc2024/day2.rs
@@ -4,6 +4,31 @@ use anyhow::Result;
 
 use crate::util::*;
 
+fn is_part_of_sequence(a: i8, b: i8, sign: i8) -> bool {
+    let diff = a - b;
+    if diff.signum() != sign {
+        return false;
+    }
+    let abs_diff = diff.abs();
+    abs_diff >= 1 && abs_diff <= 3
+}
+
+fn valid_with_skips(prev: i8, rest: &[i8], sign: i8, skipped: bool) -> bool {
+    if rest.len() == 0 {
+        return true;
+    }
+    let is_rest_match = rest.len() == 1 || is_part_of_sequence(rest[1], rest[0], sign);
+    if !is_rest_match && skipped {
+        return false;
+    }
+    if is_rest_match {
+        valid_with_skips(rest[0], &rest[1..], sign, skipped)
+    } else {
+        valid_with_skips(prev, &rest[1..], sign, true)
+            || valid_with_skips(rest[0], &rest[2..], sign, true)
+    }
+}
+
 pub struct Day2;
 
 impl Solution for Day2 {
@@ -15,6 +40,10 @@ impl Solution for Day2 {
 
     fn default_input() -> Result<Vec<u8>> {
         read_bytes!("inputs/aoc2024/day2.txt")
+        // r"80 82 81 82 83 85 88".as_bytes().to_owned().ok()
+        //   ^^
+        //      XX
+        //   XX
     }
 
     fn parse(input: &Vec<u8>) -> Result<Self::Input> {
@@ -28,16 +57,13 @@ impl Solution for Day2 {
     fn p1(xs: Self::Input) -> Result<impl Debug> {
         let mut count = 0;
         'outer: for x in xs {
-            let sign = (x[1] - x[0]).signum();
-            for (i, n) in x.iter().skip(1).enumerate() {
-                let diff = n - x[i];
-                if diff.signum() != sign {
+            let sign = (x[x.len() - 1] - x[0]).signum();
+            let mut prev = x[0];
+            for &n in x.iter().skip(1) {
+                if !is_part_of_sequence(n, prev, sign) {
                     continue 'outer;
                 }
-                let abs_diff = diff.abs();
-                if abs_diff < 1 || abs_diff > 3 {
-                    continue 'outer;
-                }
+                prev = n;
             }
             count += 1;
         }
@@ -46,23 +72,17 @@ impl Solution for Day2 {
 
     fn p2(xs: Self::Input) -> Result<impl Debug> {
         let mut count = 0;
-        'outer: for x in xs {
-            for (i, _) in x.iter().enumerate() {
-                let x = x.iter().take(i).chain(x.iter().skip(i + 1)).collect_vec();
-                let diffs = x
-                    .iter()
-                    .skip(1)
-                    .enumerate()
-                    .map(|(i, n)| *n - x[i])
-                    .collect_vec();
-                if !diffs.iter().all(|n| *n < 0) && !diffs.iter().all(|n| *n > 0) {
-                    continue;
-                }
-                if !diffs.iter().all(|n| n.abs() >= 1 && n.abs() <= 3) {
-                    continue;
-                }
+        for x in xs {
+            let sign = (x[x.len() - 1] - x[0]).signum();
+            let is_first_valid = is_part_of_sequence(x[1], x[0], sign);
+            let is_valid = if is_first_valid {
+                valid_with_skips(x[0], &x[1..], sign, false)
+            } else {
+                valid_with_skips(x[0], &x[2..], sign, true)
+                    || valid_with_skips(x[1], &x[2..], sign, true)
+            };
+            if is_valid {
                 count += 1;
-                continue 'outer;
             }
         }
         Ok(count)
